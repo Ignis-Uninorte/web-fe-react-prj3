25e53e5a0447b3a307a8a19a8f5b835f9211884b
Tabata Llach <tllach@uninorte.edu.co>
2024-11-04 14:33:57 -0500

    feat(HU2001): fix fetch problem

--- src/pages/moduleClients/main.tsx
+++ src/pages/moduleClients/main.tsx
@@ -5,18 +5,27 @@ import { useAllClients } from '../../hooks/useClients';
 import { useNavigate } from 'react-router-dom';
 
 const ModuleClient: React.FC = () => {
-    const {data: clients} = useAllClients();
-    console.log(clients);
+    const {isLoading, isSuccess, isError, data: clients} = useAllClients();
     const navigate = useNavigate();
     return (
         <div>
-            <h1>Clientes</h1>
-            <button onClick={() => navigate('/crear-cliente')} className="btn_crearcliente">Crear Cliente</button>
-                <div className="body_moduleClient">
-                    <ClientTable clients={clients} />
+            {isLoading && (
+                <p>Cargando...</p>
+            )}
+            {isError && (
+                <p>Hubo un error</p>
+            )}
+            {!isLoading && !isError && isSuccess && clients &&  (
+                <div>
+                    <h1>Clientes</h1>
+                    <button onClick={() => navigate('/crear-cliente')} className="btn_crearcliente">Crear Cliente</button>
+                        <div className="body_moduleClient">
+                            <ClientTable clients={clients} />
+                        </div>
                 </div>
+            )}
         </div>
-    );
+);
 }
 
 export default ModuleClient;
