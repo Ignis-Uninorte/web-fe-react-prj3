e14845d7f83604e58bf82716b438fde7b3a26201
Tabata Llach <tllach@uninorte.edu.co>
2024-11-04 14:33:57 -0500

    feat(HU2001): fix fetch problem

--- src/pages/moduleClients/main.tsx
+++ src/pages/moduleClients/main.tsx
@@ -5,36 +5,16 @@ import { useAllClients } from '../../hooks/useClients';
 import { useNavigate } from 'react-router-dom';
 
 const ModuleClient: React.FC = () => {
-    const clients = [
-        //Se debe cambiar para recibir los clientes de la api (preguntar a backend)
-        { id: 1, 
-            nit: 123456789,
-            name: 'Juan Perez', 
-            direction: 'Calle 123',
-            city: 'Bogota',
-            country: 'Colombia',
-            email: 'juan@example.com', 
-            activo: true },
-        { id: 2,
-            nit: 987654321,
-            name: 'Maria Rodriguez', 
-            direction: 'Calle 456',
-            city: 'Medellin',
-            country: 'Colombia',
-            email: 'a@a.com',
-            activo: false },
-    ];
-
+    const {data: clients} = useAllClients();
+    console.log(clients);
     const navigate = useNavigate();
     return (
         <div>
             <h1>Clientes</h1>
-            <button onClick={() => navigate('/crear-cliente')}>Crear Cliente</button>
+            <button onClick={() => navigate('/crear-cliente')} className="btn_crearcliente">Crear Cliente</button>
                 <div className="body_moduleClient">
-                    <h1>Listado de Clientes</h1>
                     <ClientTable clients={clients} />
                 </div>
-            )}
         </div>
 );
 }
