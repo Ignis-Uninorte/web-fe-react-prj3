8b49c874759ba3d9efd326a794fb0b3be08a71f5
Tabata Llach <tllach@uninorte.edu.co>
2024-11-04 13:59:11 -0500

    feat(HU2001): change clients mainPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,17 @@ import './App.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CreateClient from './pages/moduleClients/components/CreateClient';
-import ModuleClient from './pages/moduleClients/main';
-import ClientDetail from './pages/moduleClients/components/ClientDetail'; 
-
-
-const queryClient = new QueryClient();
-
+import ModuleClient from './pages/moduleClients/main'
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<ModuleClient />} />
-          <Route path="/crear-cliente" element={<CreateClient />} />
-          <Route path="/client/:clientId" element={<ClientDetail />} />
-        </Routes>
-      </Router>
-    </QueryClientProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/crear-cliente" element={<CreateClient />} />
+        <Route path="/listado-clientes" element={<ModuleClient />} />
+      </Routes>
+    </Router>
   );
 }
 
