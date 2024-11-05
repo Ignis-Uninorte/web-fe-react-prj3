d861fd2abeef90de51eafd55ecd49275913f0357
Camilo Heras <caheras@uninorte.edu.co>
2024-11-04 19:20:42 -0500

    feat(HU2001): add client detail page and navigation from client table

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CreateClient from './pages/moduleClients/components/CreateClient';
 import ModuleClient from './pages/moduleClients/main';
+import ClientDetail from './pages/moduleClients/components/ClientDetail'; 
+
 
 const queryClient = new QueryClient();
 
@@ -14,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<ModuleClient />} />
           <Route path="/crear-cliente" element={<CreateClient />} />
+          <Route path="/client/:clientId" element={<ClientDetail />} />
         </Routes>
       </Router>
     </QueryClientProvider>
--- src/pages/moduleClients/components/clientTable.tsx
+++ src/pages/moduleClients/components/clientTable.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import {ListClients} from '../../../types/clientes.type'
 import '../../../styles/clientTable.css';
 
 const ClientTable: React.FC<ListClients> = ({ clients }) => {
+    const navigate = useNavigate();
+    // Handler function to navigate to the client detail page
+    const handleNameClick = (clientName: string) => {
+        navigate(`/client/${encodeURIComponent(clientName)}`); // Encodes the name for the URL
+    };
     return (
         <div className="table-container">
             <table className="client-table">
@@ -21,7 +27,15 @@ const ClientTable: React.FC<ListClients> = ({ clients }) => {
                 <tr key={client.nit}>
                     <td>{client.id}</td>
                     <td>{client.nit}</td>
-                    <td>{client.name}</td>
+                    <td>
+                                {/* Make the name clickable */}
+                                <button 
+                                    onClick={() => handleNameClick(client.name)}
+                                    className="client-name-link" // Optional: you can style this as a link
+                                >
+                                    {client.name}
+                                </button>
+                            </td>
                     <td>{client.corporateEmail}</td>
                     <td>{client.active ? 'Si' : "No"}</td>
                     <td>
