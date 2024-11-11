f3c9931825f910413935d8314031ceae13947413
caheras <98917716+caheras@users.noreply.github.com>
2024-11-11 00:13:43 -0500

    Implement dropdown menu in header for navigation and enhance styles

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,37 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Import the Link component from react-router-dom
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import CompanyLogo from '../assets/CompanyLogo.svg';
 import SideMenu from '../assets/Sidemenu.svg';
 import User from '../assets/User.svg';
 import '../styles/Header.css';
 
-
 const Header: React.FC = () => {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+  const navigate = useNavigate();
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
   return (
     <header>
       <div className="header-container">
-        {/* Sidemenu */}
-        <div className="side-menu">
+        <div className="side-menu" onClick={toggleDropdown}>
           <div className="menu-icon">
             <img src={SideMenu} alt="Menu" />
           </div>
+          {dropdownVisible && (
+            <div className="dropdown-menu">
+              <ul>
+                <li onClick={() => navigate('/dashboard')}>Dashboard</li>
+                <li onClick={() => navigate('/')}>Clientes</li>
+                <li onClick={() => navigate('/oportunidades')}>Oportunidades</li>
+                <li onClick={() => navigate('/seguimiento')}>Seguimiento</li>
+              </ul>
+            </div>
+          )}
         </div>
 
-        {/* Logo */}
         <div className="logo">
           <Link to="/">
             <img src={CompanyLogo} alt="Company Logo" />
           </Link>
         </div>
 
-        {/* Account */}
         <div className="account-cart">
           <a className="account-link">
             <img src={User} alt="My Account" />
             <span>My Account</span>
           </a>
         </div>
-      </div>     
+      </div>
     </header>
   );
 };
